38f35036eaf500025191e5657621938c4e2f8470
karthick7204 <karthicklimited1234@gmail.com>
2025-03-26 11:26:17 +0530

    created a componentsand basic starter files

---
+++ fitness-studio/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+//rfc
+import React from 'react'
+
+export default function hero() {
+  return (
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
+        <p>It's time to get</p>
+        <h1>Your Belly <span>FIT</span></h1>
+        <p>Transform your fitness journey with <span>FitTrack</span>, the all-in-one app designed to help you stay consistent, track progress, and <span>achieve your fitness goals</span></p>
+    </div>
+  )
+}
