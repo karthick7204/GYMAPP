743a40fe4e170e2f84a94519ba0f7e1e2220a81d
karthick7204 <karthicklimited1234@gmail.com>
2025-04-01 17:27:23 +0530

    Update Workout.jsx

--- fitness-studio/src/components/Workout.jsx
+++ fitness-studio/src/components/Workout.jsx
@@ -5,7 +5,7 @@ import ExerciseCard from './ExerciseCard'
 export default function Workout(props) {
     const { workout } = props
     return (
-        <SectionWrapper id={'workout'} header={"welcome to"} title={['The', 'DANGER', 'zone']}>
+        <SectionWrapper id={'workout'} header={"welcome to"} title={['The', 'FOCUS', 'zone']}>
             <div className='flex flex-col gap-4'>
                 {workout.map((exercise, i) => {
                     return (
